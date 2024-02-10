f1dea5795c8c5a8eb3ef7fb6a62d261dfd30af88
devprincefahad <1pfchouhan@gmail.com>
2024-02-10 16:03:01 +0530

    Hilt changes

--- app/src/main/java/dev/prince/prodspec/MainActivity.kt
+++ app/src/main/java/dev/prince/prodspec/MainActivity.kt
@@ -6,18 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 import dev.prince.prodspec.ui.theme.ProdSpecTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ProdSpecTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
---
+++ app/src/main/java/dev/prince/prodspec/ProdSpecApplication.kt
@@ -0,0 +1,7 @@
+package dev.prince.prodspec
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ProdSpecApplication : Application()
